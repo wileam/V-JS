e1811b4af52b53e69d7fe5dd5214ed82b950353c
wileam <wmy1011@gmail.com>
2014-05-31 22:37:55 +0800

    add switch link effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // created by wileam. 2014-05-31
+$(document).ready(function() {
+  //click links
+  $('.nav.nav-list a').on('click', function(event) {
+    $('#sidebar').find('.active').removeClass('active');
+    $(this).parent('li').addClass('active');
+  });
+});
